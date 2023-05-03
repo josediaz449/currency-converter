b438f73fce92a18bfc2a308b452fb49b74b1558a
josediaz449 <josediaz449@yahoo.com>
2023-05-03 13:51:26 -0400

    Updated theme folder name. Updated Depreciated Code.

--- src/main/java/com/example/application/data/ExchangeData.java
+++ src/main/java/com/example/application/data/ExchangeData.java
@@ -7,6 +7,8 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -20,10 +22,9 @@ public class ExchangeData {
             HttpURLConnection request = connectToCodesAPI();
             request.connect();
             // Convert to JSON
-            JsonParser jp = new JsonParser();
-            JsonElement root = jp.parse(new InputStreamReader((InputStream) request.getContent()));
-            JsonObject jsonobj = root.getAsJsonObject();
-            JsonArray codesObj = jsonobj.getAsJsonArray("supported_codes");
+            JsonElement root = JsonParser.parseReader(new InputStreamReader((InputStream) request.getContent()));
+            JsonObject jsonObject = root.getAsJsonObject();
+            JsonArray codesObj = jsonObject.getAsJsonArray("supported_codes");
             for(JsonElement e : codesObj){
                 Currency c = new Currency(e.getAsJsonArray().get(1).getAsString(), e.getAsJsonArray().get(0).getAsString(),1);
                 currencies.add(c);
@@ -33,16 +34,19 @@ public class ExchangeData {
             ex.printStackTrace();
         }
     }
-    public static HttpURLConnection connectToCodesAPI() throws IOException {
+    public static HttpURLConnection connectToCodesAPI() throws IOException, URISyntaxException {
         String url_str = "https://v6.exchangerate-api.com/v6/1ece87b59a264caa4a79fb4f/codes";
         // Making Request
-        URL url = new URL(url_str);
+        URI URI = new URI(url_str);
+        URL url = URI.toURL();
         return  (HttpURLConnection) url.openConnection();
     }
-    public static HttpURLConnection connectToPairAPI(String base, String target) throws IOException {
+    public static HttpURLConnection connectToPairAPI(String base, String target) throws IOException, URISyntaxException {
         String url_str = "https://v6.exchangerate-api.com/v6/1ece87b59a264caa4a79fb4f/pair/"+base+"/"+target;
         // Making Request
-        URL url = new URL(url_str);
+        URI URI = new URI(url_str);
+        URL url = URI.toURL();
+
         return  (HttpURLConnection) url.openConnection();
     }
     public static double getConversion(String base, String target){
@@ -59,10 +63,9 @@ public class ExchangeData {
             HttpURLConnection request = connectToPairAPI(base,target);
             request.connect();
             // Convert to JSON
-            JsonParser jp = new JsonParser();
-            JsonElement root = jp.parse(new InputStreamReader((InputStream) request.getContent()));
-            JsonObject jsonobj = root.getAsJsonObject();
-            conversion = jsonobj.get("conversion_rate").getAsDouble();
+            JsonElement root = JsonParser.parseReader(new InputStreamReader((InputStream) request.getContent()));
+            JsonObject jsonObject = root.getAsJsonObject();
+            conversion = jsonObject.get("conversion_rate").getAsDouble();
             if(keyExists){
                 conversionMap.get(base).put(target,new Currency(target,conversion));
                 HashMap<String,Currency> mapTarget = new HashMap<>();
@@ -77,7 +80,7 @@ public class ExchangeData {
                 conversionMap.get(base).put(target,new Currency(target,conversion));
                 conversionMap.get(target).put(base,new Currency(base,(double)1/conversion));
             }
-            System.out.println(jsonobj.get("base_code").getAsString()+"-->"+jsonobj.get("target_code").getAsString()+" "+jsonobj.get("conversion_rate").getAsDouble());
+            System.out.println(jsonObject.get("base_code").getAsString()+"-->"+jsonObject.get("target_code").getAsString()+" "+jsonObject.get("conversion_rate").getAsDouble());
             System.out.println(conversionMap.toString());
 
         }
